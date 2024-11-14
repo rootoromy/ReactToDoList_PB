9fa5fa71092d22ee74ea1cf624dbf7d0374f52ec
bu-yasutake <h.yasutake@brewus.co.jp>
2024-11-14 17:16:51 +0900

    index.htmlファイルを移動

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   base: '/ReactToDoList_PB/'
+  base: '/ReactToDoList_PB/',
+  plugins: [react()]
 })
